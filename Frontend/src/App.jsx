0810994e5fa59dc7741f43cf354f8a8a1bcd5c3b
raviraj-47 <ravirajjio123@gmail.com>
2026-03-12 21:22:28 +0530

    Add Training Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import DriverDashboard from './pages/DriverDashboard/DriverDashboard'
 import { Route, Routes } from 'react-router-dom'
+import DriverDashboard from './pages/DriverDashboard/DriverDashboard'
 import LoginPage from './pages/LoginPage/LoginPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
+
+// 1. Import the new Training Page
+import TrainingPage from './pages/TrainingPage/TrainingPage'
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
         <Route path='/driverDashboard' element={<DriverDashboard/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/signup' element={<RegisterPage/>}/>
+        
+        {/* 2. Add the specific route for the training page */}
+        <Route path='/training' element={<TrainingPage/>}/>
       </Routes>
     </div>
   )
